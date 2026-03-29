96bc88a93ee5179b261e82e646bed5cd68fb6898
Broyojo <dha@xoba.com>
2026-03-29 03:26:08 -0400

    v60: BFS depth 50 - full map coverage

--- agent/controller.py
+++ agent/controller.py
@@ -112,7 +112,7 @@ class PlayerController:
 
         while queue:
             r, c, first_dir, depth = queue.popleft()
-            if depth > 40:
+            if depth > 50:
                 break
 
             cell = board.cells[r][c]
@@ -149,7 +149,7 @@ class PlayerController:
                 best_priority = priority
                 best_first_dir = first_dir
 
-            if depth < 40:
+            if depth < 50:
                 for dr, dc in DR:
                     nr, nc = r + dr, c + dc
                     if (nr, nc) in visited or not valid(nr, nc):
