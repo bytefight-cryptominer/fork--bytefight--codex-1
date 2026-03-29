d1d72917b14296630c4debbcc638e1fe365b404a
Broyojo <dha@xoba.com>
2026-03-29 04:47:33 -0400

    v91: v89 + enemy territory 800 (from 1100)

--- agent/controller.py
+++ agent/controller.py
@@ -134,7 +134,7 @@ class PlayerController:
                 hill = board.hills[cell.hill_id]
                 if hill.controller_parity == self.opp:
                     if cell.owner_parity != player_parity:
-                        priority = 3000 - depth * 20
+                        priority = 2500 - depth * 20
                     else:
                         priority = 1200 - depth * 20
                 elif hill.controller_parity != player_parity:
@@ -157,7 +157,7 @@ class PlayerController:
             if cell.owner_parity == self.opp and priority < -900:
                 d_opp = mdist(r, c, opp_r, opp_c)
                 if d_opp > SAFE_DIST:
-                    priority = 1100 - depth * 20  # higher enemy territory priority
+                    priority = 800 - depth * 20  # higher enemy territory priority
 
             if priority > -900:
                 priority += count_paintable(r, c) * 2
