e923042607c1d727a9bcd8f29ae306ea80a6f71a
Broyojo <dha@xoba.com>
2026-03-29 04:14:09 -0400

    v81 CONFIRMED: 1573, 1581 - avg 1577! Skip non-hill reinforcement is a major win

--- agent/controller.py
+++ agent/controller.py
@@ -225,4 +225,4 @@ class PlayerController:
         return actions
 
     def commentate(self, board: Board, player_parity: int, time_left: Callable) -> str:
-        return ""
+        return "v81b"
