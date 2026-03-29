8a2a730f5c4114967cbe40343adf0ee6feca0055
Broyojo <dha@xoba.com>
2026-03-29 03:33:35 -0400

    v63: depth 32 + enemy traversal

--- agent/controller.py
+++ agent/controller.py
@@ -112,7 +112,7 @@ class PlayerController:
 
         while queue:
             r, c, first_dir, depth = queue.popleft()
-            if depth > 35:
+            if depth > 32:
                 break
 
             cell = board.cells[r][c]
@@ -149,7 +149,7 @@ class PlayerController:
                 best_priority = priority
                 best_first_dir = first_dir
 
-            if depth < 35:
+            if depth < 32:
                 for dr, dc in DR:
                     nr, nc = r + dr, c + dc
                     if (nr, nc) in visited or not valid(nr, nc):
@@ -217,4 +217,4 @@ class PlayerController:
         return actions
 
     def commentate(self, board: Board, player_parity: int, time_left: Callable) -> str:
-        return "d35"
+        return "d32"
